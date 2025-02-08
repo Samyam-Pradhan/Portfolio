e75e61b464df3a95f190dc3d9dba217fe0b25669
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-02-08 09:20:15 +0545

    Update Components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { FaEnvelope, FaLinkedin } from 'react-icons/fa';  
-
+import { FaEnvelope, FaLinkedin } from 'react-icons/fa';
+import "../assets/Contact.css";
 const Contact = () => {
   return (
-    <section id="contact">
+    <section className="contact">
       <div className="contact-container">
         <h2>Contact Me</h2>
         <p>If you'd like to get in touch, feel free to reach out via email or LinkedIn!</p>
         <div className="contact-links">
           <a href="mailto:pradhansamyam71@gmail.com" className="contact-link">
-            <FaEnvelope size={30} style={{ marginRight: '10px' }} /> 
-            Email: pradhansamyam71@gmail.com
+            <FaEnvelope size={30} style={{ marginRight: '10px' }} />
+            pradhansamyam71@gmail.com
           </a>
-          <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="contact-link">
-            LinkedIn:<FaLinkedin size={30} style={{ marginRight: '10px' }} />
+          <a 
+            href="https://www.linkedin.com/in/samyam-pradhan-b59b68292/" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="contact-link"
+          >
+            <FaLinkedin size={30} style={{ marginRight: '10px' }} />
+            Connect on LinkedIn
           </a>
         </div>
       </div>
